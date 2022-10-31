77ff4baeb093277c26fbfce0ea695c9707e34175
Thanh Ngoc <phanthanhngoc11@gmail.com>
2022-11-01 00:09:17 +0700

    process order 2

--- src/routes/(app)/cart/+page.server.js
+++ src/routes/(app)/cart/+page.server.js
@@ -12,36 +12,36 @@ export const load = async () => {
 
 export const actions = {
 	placeorder: async ({ request }) => {
-        const form_data = await request.formData();
+        // const form_data = await request.formData();
+        // console.log('form_data: ',form_data);
 		const form_obj = Object.fromEntries(await request.formData());
-        console.log('form_data: ',form_data);
         console.log('form_obj: ',form_obj);
 
-        // get bot info
-        const page = 1;
-        const response = await client.records.getList('telegram_bot', page, perPage, {
-            filter: 'name = "orderbot"',
-        });
+        // // get bot info
+        // const page = 1;
+        // const response = await client.records.getList('telegram_bot', page, perPage, {
+        //     filter: 'name = "orderbot"',
+        // });
 
-        const bots = await serializeNonPOJOs(response);
-        if (bots.totalItems !== 1) {
-            // notifier.danger("Not found!");
-            console.log('bots: ',bots);
-            throw redirect(303, '/cart');
-        }
-        // send message
-        const send_message = await fetch(`https://api.telegram.org/bot${bots.items[0].bot_key}/sendMessage`,{
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            method: 'POST',
-            body: JSON.stringify({
-                'chat_id': bots.items[0].group_id,
-                'text': 'New order placed!'
-            })
-        })
-        const message = await send_message.json()
-        // console.log('message: ',message);
+        // const bots = await serializeNonPOJOs(response);
+        // if (bots.totalItems !== 1) {
+        //     // notifier.danger("Not found!");
+        //     console.log('bots: ',bots);
+        //     throw redirect(303, '/cart');
+        // }
+        // // send message
+        // const send_message = await fetch(`https://api.telegram.org/bot${bots.items[0].bot_key}/sendMessage`,{
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //     },
+        //     method: 'POST',
+        //     body: JSON.stringify({
+        //         'chat_id': bots.items[0].group_id,
+        //         'text': 'New order placed!'
+        //     })
+        // })
+        // const message = await send_message.json()
+        // // console.log('message: ',message);
 
 
         // const record = await client.records.create('customer_order', {
